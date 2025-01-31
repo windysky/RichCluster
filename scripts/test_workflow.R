38ccfcc759a8d89cc7eac9ea10b9c846a55e8286
sarah <ssh2198@columbia.edu>
2025-01-30 21:23:36 -0500

    clean test workflow

--- scripts/test_workflow.R
+++ scripts/test_workflow.R
@@ -2,72 +2,41 @@
 load_cluster_data <- function(from_scratch=FALSE)
 {
   if (from_scratch == FALSE)  {
-    # Load the cluster data from CSV files
-    distance_matrix_df <- read.csv("inst/extdata/distance_matrix.csv", row.names=1, check.names=FALSE)
-    distance_matrix <- as.matrix(distance_matrix_df)
-    merged_seeds <- read.csv("inst/extdata/merged_seeds.csv")
-    # Make the fake cluster_result with the loaded data :^)
-    cluster_result <- list(DistanceMatrix=distance_matrix, MergedSeeds=merged_seeds)
+    # load the cluster data from the files
+    cluster_result <- readRDS('inst/extdata/cluster_result.rds')
   }
   else {
-    # Read the data files
-    # rr1 <- read.delim(system.file("extdata", "go1.txt", package="RichCluster"))
-    # rr2 <- read.delim(system.file("extdata", "go2.txt", package="RichCluster"))
-    rr1 <- read.csv(system.file("extdata", "7mo_DEG.csv", package="RichCluster"))
-    rr2 <- read.csv(system.file("extdata", "7mo_DMR.csv", package="RichCluster"))
+    # read and manually cluster
+    rr1 <- read.delim(system.file("extdata", "go1.txt", package="RichCluster"))
+    rr2 <- read.delim(system.file("extdata", "go2.txt", package="RichCluster"))
 
-    richsets <- list(rr1, rr2)
-    richnames <- c('7mo_DEG', '7mo_DMR')
-
-    # merged_richsets <- RichCluster::merge_richsets(richsets)
-    #
-    # term_vec <- merged_richsets$Term
-    # geneID_vec <- merged_richsets$GeneID
-    # padj_vec <- merged_richsets$Padj
+    enrichment_results <- list(rr1, rr2)
+    rr_names <- c('7mo_DEG', '7mo_DMR')
 
     cluster_result <- RichCluster::cluster(
-      richsets, richnames,
-      "kappa", 0.8,
-      "DAVID", 0.8
+      enrichment_results, df_names=rr_names, min_terms=5,
+      distance_metric="kappa", distance_cutoff=0.5,
+      merge_strategy="DAVID", membership_cutoff=0.5
     )
-
-    # Save cluster results to CSV for faster future testing
-    dist_matrix_df <- as.data.frame(cluster_result$distance_matrix)
-    write.csv(dist_matrix_df, "inst/extdata/distance_matrix.csv", row.names=TRUE)
-    write.csv(cluster_result$all_clusters, "inst/extdata/clusters.csv", row.names=FALSE)
-
+    saveRDS(cluster_result, file = "inst/extdata/cluster_result.rds")
   }
   return(cluster_result)
 }
 
-# Example of what workflow using the functions looks like
+# example of what workflow using the functions looks like
 test_workflow <- function(cluster_result, min_terms=5) {
-  # Unpack distance_matrix and merged_seeds from list
-  distance_matrix <- cluster_result$distance_matrix
-  all_clusters <- cluster_result$all_clusters
-
-  final_clusters <- RichCluster::filter_merged_seeds(cluster_result$all_clusters, 3)
-  full_clusterdf <- RichCluster::make_full_clusterdf(final_clusters, merged_richsets)
-
-  if (FALSE) {
-    # Sanity check: Does summary_data look okay
-    summary_data <- RichCluster::make_summary_data(full_clusterdf, "Padj")
-    print(nrow(summary_data))
-  }
 
   # Testing: Choose one to comment out and test
-  result <- RichCluster::all_clusters_hmap(full_clusterdf, "Padj")
+  # result <- RichCluster::all_clusters_hmap(full_clusterdf, "Padj")
   # result <- RichCluster::cluster_correlation_hmap(final_clusters, distance_matrix, 3)
-  # result <- RichCluster::cluster_network(final_clusters, distance_matrix, 1)
+  result <- RichCluster::cluster_network(final_clusters, distance_matrix, 1)
   # result <- RichCluster::full_network(distance_matrix[1:30, 1:30])
   return(result)
+
 }
 
 cluster_result <- load_cluster_data(from_scratch=TRUE)
 # cluster_result <- load_cluster_data(from_scratch=FALSE)
 
-all_hmap_5 <- test_workflow(cluster_result, 3)
-all_hmap_5
-
-all_hmap_10 <- test_workflow(cluster_result, 10)
-all_hmap_10
+hmap <- test_workflow(cluster_result, 10)
+hmap
